abf302dbb044864826b386c50e949395e5e76f47
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2019-03-08 18:24:35 +0200

    d

---
+++ src/components/table.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Table = (props) => {
+  return (
+      <table></table>
+  )
+}
+
+export default Table
---
+++ src/components/tbody.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Tbody = (props) => {
+  return (
+      <tbody></tbody>
+  );
+};
+
+export default Tbody;
