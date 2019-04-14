9ddeb8155abd8dfe70f5f273e7487146686a67e3
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2019-04-15 02:39:34 +0300

    upload

--- src/views/Upload.js
+++ src/views/Upload.js
@@ -1,60 +1,55 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Button} from '@material-ui/core';
-import {Send} from '@material-ui/icons';
-import red from '@material-ui/core/colors/red';
-
-import {withStyles} from '@material-ui/core/styles';
-
-const styles = theme => ({
-  container: {
-    width: '50%',
-    padding: '1rem',
-  },
-  button: {
-    margin: theme.spacing.unit,
-  },
-  alert: {
-    color: red[500],
-  },
-});
+import {ValidatorForm, TextValidator} from 'react-material-ui-form-validator';
+import {Button, CircularProgress, Typography} from '@material-ui/core';
+import TextField from '@material-ui/core/es/TextField/TextField';
+import './css/Upload.css';
+import {Slider} from '@material-ui/lab';
 
 class Upload extends Component {
+  mediaUrl = 'http://media.mw.metropolia.fi/wbma/';
 
-  apiURL = 'http://media.mw.metropolia.fi/wbma/media';
+  fr = new FileReader();
 
-  form = React.createRef();
+  componentDidMount() {
+    this.fr.addEventListener('load', () => {
+      this.setState((prevState) => ({
+        ...prevState,
+        imageData: this.fr.result,
+      }));
+    });
+  }
 
   state = {
     file: {
       title: '',
       description: '',
-      data: '',
+      filedata: null,
+      filename: undefined,
     },
-    message: '',
+    loading: false,
+    imageData: null,
+    filters: {
+      brightness: 100,
+      contrast: 100,
+      warmth: 0,
+      saturation: 100,
+    },
+    type: '',
   };
 
-  handleUploadSubmit = (evt) => {
-    evt.preventDefault();
-    const fd = new FormData();
-    console.log(fd);
-    fd.append('title', this.state.file.title);
-    fd.append('description', this.state.file.description);
-    fd.append('file', this.state.file.data);
-    const settings = {
-      method: 'POST',
-      headers: {
-        'x-access-token': localStorage.getItem('token'),
+  handleFileChange = (evt) => {
+    evt.persist();
+    console.log(evt.target.files[0]);
+    this.fr.readAsDataURL(evt.target.files[0]);
+    this.setState((prevState) => ({
+      ...prevState,
+      type: evt.target.files[0].type,
+      file: {
+        ...prevState.file,
+        filedata: evt.target.files[0],
       },
-      body: fd,
-    };
-    fetch(this.apiURL, settings).then((response) => {
-      return response.json();
-    }).then((result) => {
-      console.log(result);
-      this.props.updateImages();
-      this.props.history.push('/home');
-    });
+    }));
   };
 
   handleInputChange = (evt) => {
@@ -68,61 +63,138 @@ class Upload extends Component {
       file: {
         ...prevState.file,
         [name]: value,
-
       },
     }));
-
   };
 
-  handleFileChange = (evt) => {
-    evt.persist();
-    console.log(evt.target.files[0]);
+  handleFileSubmit = (evt) => {
+    console.log(evt);
+    this.setState({loading: true});
+    const fd = new FormData();
+    fd.append('title', this.state.file.title);
+    const description  = `[d]${this.state.file.description}[/d][f]${JSON.stringify(this.state.filters)}[/f]`
+    fd.append('description', description);
+    fd.append('file', this.state.file.filedata);
+
+    const options = {
+      method: 'POST',
+      body: fd,
+      headers: {
+        'x-access-token': localStorage.getItem('token'),
+      },
+    };
+
+    fetch(this.mediaUrl + 'media', options).then(response => {
+      return response.json();
+    }).then(json => {
+      console.log(json);
+      setTimeout(() => {
+        this.props.history.push('/home');
+        this.props.updateImages();
+        this.setState({loading: false});
+      }, 2000);
+
+    });
+  };
+
+  rangeReducer = (rawValue, props) => {
+    console.log(rawValue);
+    const {name} = props;
+    const value = Math.round(rawValue);
     this.setState((prevState) => ({
-      file: {
-        ...prevState.file,
-        data: evt.target.files[0],
+      filters: {
+        ...prevState.filters,
+        [name]: value,
       },
     }));
   };
 
   render() {
-    const {classes} = this.props;
     return (
-        <div className={classes.container}>
-          <h1>Register</h1>
-          <form onSubmit={this.handleUploadSubmit}>
-            <input name="title" id="title"
-                   label="Title"
-                   value={this.state.file.title}
-                   onChange={this.handleInputChange}/>
-            <textarea name="description"
-                      id="description"
-                      label="Description"
-                      value={this.state.file.description}
-                      onChange={this.handleInputChange}></textarea>
-            <input name="file"
-                   id="file"
-                   label="File"
-                   type="file"
-                   value={this.state.file.filename}
-                   onChange={this.handleFileChange}/>
-            <Button className={classes.button} variant="contained"
-                    color="primary" type="submit">
-              <Send/>&nbsp;Upload
-            </Button>
-          </form>
-          <p className={classes.alert}>
-            {this.state.message}
-          </p>
-        </div>
-    );
+        <React.Fragment>
+          <h1>Upload</h1>
+          <ValidatorForm instantValidate={false}
+                         onSubmit={this.handleFileSubmit}
+                         onError={errors => console.log(errors)}>
+            <TextValidator name="title" label="Title"
+                           value={this.state.file.title}
+                           onChange={this.handleInputChange}
+                           validators={['required', 'minStringLength:3']}
+                           errorMessages={[
+                             'this field is required',
+                             'minimum 3 charaters']}
+                           fullWidth/>
+            <TextValidator name="description" label="Description"
+                           value={this.state.file.description}
+                           onChange={this.handleInputChange}
+                           validators={['required', 'minStringLength:3']}
+                           errorMessages={[
+                             'this field is required',
+                             'minimum 3 charaters']}
+                           fullWidth
+                           multiline
+                           rows={3}/>
+            <TextField name="filedata" label="File"
+                       value={this.state.file.filename}
+                       type="file"
+                       onChange={this.handleFileChange}
+                       fullWidth/>
+            <Button type="submit" variant="contained"
+                    color="primary">Upload&nbsp;{this.state.loading &&
+            <CircularProgress size={20}/>}</Button>
+          </ValidatorForm>
+          {this.state.imageData !== null && this.state.type.includes('image') &&
+          <div><img src={this.state.imageData} alt="preview"
+                    className={'preview'}
+                    style={{filter: `brightness(${this.state.filters.brightness}%) contrast(${this.state.filters.contrast}%) sepia(${this.state.filters.warmth}%) saturate(${this.state.filters.saturation}%)`}}/>
+            <div>
+              <Typography id="brightness-label">Brightness: {this.state.filters.brightness}%</Typography>
+              <Slider name="brightness" value={this.state.filters.brightness}
+                      valueReducer={this.rangeReducer}
+                      min={0}
+                      max={200}
+                      step={1}
+                      aria-labelledby="brightness-label"/>
+            </div>
+            <div>
+              <Typography id="contrast-label">Contrast: {this.state.filters.contrast}%</Typography>
+              <Slider name="contrast" value={this.state.filters.contrast}
+                      valueReducer={this.rangeReducer}
+                      min={0}
+                      max={200}
+                      step={1}
+                      aria-labelledby="contrast-label"/>
+            </div>
+            <div>
+              <Typography id="saturation-label">Saturation: {this.state.filters.saturation}%</Typography>
+              <Slider name="saturation" value={this.state.filters.saturation}
+                      valueReducer={this.rangeReducer}
+                      min={0}
+                      max={200}
+                      step={1}
+                      aria-labelledby="saturation-label"/>
+            </div>
+            <div>
+              <Typography id="warmth-label">Warmth: {this.state.filters.warmth}%</Typography>
+              <Slider name="warmth" value={this.state.filters.warmth}
+                      valueReducer={this.rangeReducer}
+                      min={0}
+                      max={100}
+                      step={1}
+                      aria-labelledby="warmth-label"/>
+            </div>
+          </div>
+          }
+
+        </React.Fragment>
+    )
+        ;
   }
 }
 
 Upload.propTypes = {
   history: PropTypes.object,
-  classes: PropTypes.object,
   updateImages: PropTypes.func,
 };
 
-export default withStyles(styles)(Upload);
+export default Upload;
